2a9718dfb311ab9de2eacc27601a8d4cdd8184c5
Martin Puttke <martinstefanputtke@posteo.de>
2020-12-06 11:47:51 +0100

    create a package

--- ecrib.el
+++ ecrib.el
@@ -1,3 +1,47 @@
+;;; ecrib.el --- A simple key-value store
+
+;; Copyright (C) 2010-2020 Martin Puttke
+
+;; Author: Martin Puttke <m.s.p@posteo.de>
+;; Created: 06 Dec 2020
+;; Keywords: convenience
+;; URL: https://github.com/matlantis/emacs-ecrib
+;; Version: 0.1
+;; Package-Requires: (helm)
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; The Package provides a place to store key-value pairs in global and
+;; persistent location.
+;;
+;; Essential Commands:
+;; - ecrib-add: add a new key value pair
+;; - ecrib-helm: browse key-value pairs with helm. There are helm actions
+;; defined for insertion, opening, editing and deletion of selected keys
+;; (C-z in helm buffer so see them).
+
+;;; Change Log:
+;;
+;; empty
+
+;;; Code:
+
+(require 'helm)
+(require 'helm-source)
+
 ;; read cribs from file
 (defun my-file-contents (filename)
   "Return the contents of FILENAME."
@@ -44,6 +88,7 @@
   (kill-new (cdr (assoc key ecrib-definitions))))
 
 ;; interactive stuff
+;;;###autoload
 (defun ecrib-add ()
   (interactive)
   (let* ((value (read-string "New ecrib value: "))
@@ -78,13 +123,17 @@
     (dolist (entry ecrib-definitions value)
       (setq value (cons (cons (concat (car entry) " (" (cdr entry) ")") (car entry)) value)))))
 
+;;;###autoload
 (defun ecrib-helm ()
   (interactive)
   (helm :sources (helm-build-sync-source "helm-ecrib-source"
                    :candidates (ecrib-helm-candidates)
-                   :action '(("Copy value to kill-ring" . ecrib-kill-new)
-                             ("Insert value at point" . ecrib-insert)
+                   :action '(("Insert value at point" . ecrib-insert)
+                             ("Copy value to kill-ring" . ecrib-kill-new)
                              ("Edit" . ecrib-edit)
                              ("Delete" . ecrib-delete))
                    :match-on-real t)
         :buffer "*helm ecrib*"))
+
+(provide 'ecrib)
+;;; ecrib.el ends here
